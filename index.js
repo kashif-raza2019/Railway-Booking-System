b038ca473372aea3efba617255b35e21d5c7b499
Kashif Raza <kashifraza08012001@gmail.com>
2021-11-22 15:00:56 +0530

    Third Commit

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // Required Files
 const express = require('express');
+const bodyParser = require('body-parser');
+const trainDetails = require('./js/user.js');
 // Port Number
 const port = process.env.PORT || 3000;
 const app = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+
+// form filling for train
+app.post('/train', urlencodedParser, function(req, res) {
+    // Prepare output in JSON format
+    response = {
+        trainNumber: req.body.train,
+        trainName: trainDetails.getTrainDetails(req.body.train),
+        dateOfTravel: req.body.date,
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
 
 app.listen(port,function() {
     console.log(`Listening on port ${port}`)
--- js/user.js
+++ js/user.js
@@ -1,13 +1,31 @@
-const trainNumber = ["19323", "20432"];
-const trainName = ["Indore-Nagpur Superfast", "Indore-Yashwantpur Superfast Express"];
-const trainCode = ["INDB-NGP", "INDB-YSP"];
+/**
+ * Train Coach Detail Chart-Mapping
+ */
 
-// Suggestion Function for Train on Key Up
-function suggestTrainOnKeyUp() {
-    const train = document.getElementById("train").value;
-    const trainSuggestion = document.getElementById("train");
-    
-    
-}
+const trainDetails = [
+    { trainName: "Indore Nagpur Trishatabdi Suoerfast Express", trainNo: "12913", trainSource: "Indore", trainDestination: "Nagpur", typeOfCoaches : "4", numberOFCoaches: 16  },
+    { trainName: "Puri Hamsafar Express", trainNo: "19317", trainSource: "Indore", trainDestination: "Puri", typeOfCoaches : "5", numberOFCoaches: 18 },
+    { trainName: "Indore Yesvantpur Express", trainNo: "19301", trainSource: "Indore", trainDestination: "yesvantpur", typeOfCoaches : "5", numberOFCoaches: 18  },
+    { trainName: "HZT. NZM-KSR Banglore Rajdhani Express", trainNo: "02692", trainSource: "NZM-Delhi", trainDestination: "Banglore City", typeOfCoaches : "5", numberOFCoaches: 18  },
+];
 
-document.getElementById("train").addEventListener("keyup", suggestTrainOnKeyUp);
+const trainNumbers = [ "12913", "19317", "19301", "02692" ];
+const trainName = [ "Indore Nagpur Trishatabdi Suoerfast Express", "Puri Hamsafar Express", "Indore Yesvantpur Express", "HZT. NZM-KSR Banglore Rajdhani Express" ];
+
+const coachDetails = [
+    { coachType: "1AC", coachName: "AC First Tier", coachFare: "Rs 4999", coachSeats: "20" },
+    { coachType: "2AC", coachName: "AC Second Tier", coachFare: "Rs 2999", coachSeats: "48" },
+    { coachType: "3AC", coachName: "AC Third Tier", coachFare: "Rs 1999", coachSeats: "64" },
+    { coachType: "SL", coachName: "Sleeper", coachFare: "Rs 999", coachSeats: "72" },
+    { coachType: "CC", coachName: "Chair Class", coachFare: "Rs 499", coachSeats: "84" }
+];
+
+function getTrainDetails(trainNo) {
+    for(let i=0; i<trainNumbers.length; i++) {
+        if(trainNumbers[i] == trainNo) {
+            return trainName[i];
+        }
+    }
+};
+
+module.exports = { getTrainDetails, trainDetails, coachDetails };
